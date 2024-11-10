e43e0d3319435b05a8565a3bf11cc98f737bb406
Karan Parashar <karan@pixelos.net>
2024-11-11 00:32:02 +0530

    Initialize backend

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
 # To do 
 
-- [ ] Make frontend and backend
-- [ ] Find a better movie search system
-- [ ] Make model faster
+- [ ] Make frontend 
+- [x] Make backend
+- [ ] Implement fuzzy search for movies
+- [x] Make model faster
 - [ ] Add feature to store vectorized values
 
 
 
 ```bash
-cd spider
-scrapy crawl moviebot
-```
-
-```bash
-cd model 
-python main.py
+fastapi dev app/main.py
 ```
---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from model.main import get_hybrid_recommendations
+from dependencies import if_exists
+
+app = FastAPI()
+
+
+@app.post("/get_recommendations")
+async def get_recommendations(movie: str):
+
+    search_result = await if_exists(movie=movie)
+    if search_result:
+        print(len(search_result))
+        movie_index, movie_name = search_result
+    else:
+        return {"response": "movie not found"}
+
+    result = get_hybrid_recommendations(movie_index)
+    return result
